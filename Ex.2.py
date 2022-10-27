d6940e8af153ad1633d117370177688df2863ba8
Adri <aavila20479@iesjoanramis.org>
2022-10-27 11:59:51 +0200

    1r Commit local

---
+++ Ex.2.py
@@ -0,0 +1,10 @@
+from re import A
+
+
+a = input("pon un numero:")
+if (int(a)>18):
+    print(a, ", lo tienes mas grande que yo O_O")
+elif (int (a)<18):
+    print(a, ", nada mal, pero la mia es mas grande XD")
+else:
+    print("Respeto mi pana B)")
